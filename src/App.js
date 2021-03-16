593fca8ab6608743c60a3da680caa45f9f24a8e9
Bart Dority <b0rgBart3@gmail.com>
2021-03-16 10:33:27 -0700

    adding 404 page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
+
 
 function App() {
   return (
     <Router>
         <div className="App">
-          <Route path='/' component={HomePage}/>
+        {/* Switch statement makes sure that only one route is chosen. */}
+          <Switch>
+            <Route path='/' component={HomePage} />
+            <Route component={NotFoundPage} />
+          </Switch>
         </div>
     </Router>
    
