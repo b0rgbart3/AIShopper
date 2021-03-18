f5008d2df20e18a82a9eae91c0645c718051170b
Bart Dority <b0rgBart3@gmail.com>
2021-03-18 08:01:54 -0700

    responsive navbar

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -5,7 +5,7 @@ import './Burger.css';
 const Burger = () => (
 
     <div className='burger' onclick=''>
-        <div className='bar1'>x</div>
+        <div className='bar1'></div>
         <div className='bar2'></div>
         <div className='bar3'></div>
     </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import '../App.css';
-import './Nav.css';
-import Burger from './Burger';
+import React from "react";
+import "../App.css";
+import "./Nav.css";
+import Burger from "./Burger";
 
 const Nav = () => (
-    <header>
-       <div className='logotype'>
-        ImageShoppr
-        <span className='registered'>&reg;</span>
-       </div> 
-       <div className='desktopMenu'></div>
-       <Burger />
-    </header>
-)
+  <header>
+    <div className="logotype">
+      ImageShoppr
+      <span className="registered">&reg;</span>
+    </div>
+    <div className="desktopMenu">
+      <ul>
+        <li>Search</li>
+        <li>About</li>
+        <li>Login</li>
+      </ul>
+    </div>
+    <Burger />
+  </header>
+);
 
 export default Nav;
