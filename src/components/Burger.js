89483b86bf6c232b514c629b5f7c76e4150c2072
Bart Dority <b0rgBart3@gmail.com>
2021-03-18 09:29:15 -0700

    expanding mobile menu

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -2,14 +2,17 @@ import React from 'react';
 import '../App.css';
 import './Burger.css';
 
-const Burger = () => (
+const Burger = (props) => {
 
-    <div className='burger' onclick=''>
+
+    return (
+
+    <div className='burger' onClick={props.handler}>
         <div className='bar1'></div>
         <div className='bar2'></div>
         <div className='bar3'></div>
     </div>
 
-)
-
+);
+    }
 export default Burger;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
 import "./Nav.css";
 import Burger from "./Burger";
 
-const Nav = () => (
-  <header>
-    <div className="logo">
-      ImageShoppr
-      <span className="registered">&reg;</span>
-    </div>
-    <div className="desktopMenu">
-      <ul>
-        <li><Link className="navLink">Search</Link></li>
-        <li><Link className="navLink">About</Link></li>
-        <li><Link className='pill'>Login</Link></li>
-      </ul>
-    </div>
-    <Burger />
-  </header>
-);
+
+const Nav = () => {
+    const [mobileOpen, setMobileOpen] = useState('phase2');
+
+function toggleMobileMenu(e) {
+    e.preventDefault();
+    console.log("toggling mobile menu");
+    if (mobileOpen === '') {
+        setMobileOpen('mobileMenuOpen');
+    } else {
+        setMobileOpen('');
+    }
+}
+    return (
+        <>
+            <header>
+            <div className="logo">
+                ImageShoppr
+                <span className="registered">&reg;</span>
+            </div>
+            <div className="desktopMenu">
+                <ul>
+                <li>
+                    <Link className="navLink" to="/search">Search</Link>
+                </li>
+                <li>
+                    <Link className="navLink" to="/about">About</Link>
+                </li>
+                <li>
+                    <Link className="pill" to="/login">Login</Link>
+                </li>
+                </ul>
+            </div>
+            <Burger handler={toggleMobileMenu}/>
+            </header>
+            <div className={`mobileMenu ${mobileOpen}`}>
+            <ul>
+                <li>
+                <Link className="navLink" to="/search">Search</Link>
+                </li>
+                <li>
+                <Link className="navLink" to="/about">About</Link>
+                </li>
+                <li>
+                <Link className="pill" to="/login'">Login</Link>
+                </li>
+            </ul>
+            </div>
+        </>
+        );
+    }
 
 export default Nav;
