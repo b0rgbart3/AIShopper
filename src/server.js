23aff6a01e7e417f863c0360c4e284ee0adc363f
Bart Dority <b0rgBart3@gmail.com>
2021-03-17 17:54:56 -0700

    serving static pages

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import path from 'path';
 const shopprController = require("./controller.js");
 
 const app = express();
 
 
-
+app.use(express.static(path.join(__dirname, '/build')));
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
@@ -16,6 +17,12 @@ app.get('/api/searches', (req,res) => {
     shopprController.getAllSearches(req, res);
  //   res.send('Searches:');
 })
+
+// pass all get requests that aren't part of the API to the React App
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname + 'build/index.html'));
+});
+
 
 app.listen(8000, () => console.log('Listening on port 8000'));
 
